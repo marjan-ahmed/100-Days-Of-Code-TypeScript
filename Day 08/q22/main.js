4ea6e3807120c68b66606374ee04ee252461d23e
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2024-07-24 20:55:37 +0500

    first commit

--- Day 08/q22/main.js
+++ Day 08/q22/main.js
@@ -1,4 +1,5 @@
 "use strict";
 let friends = ["Salman", "Da'ood", "Yousuf"];
 console.log(friends[3]); // Intentional error (Out Of Bound Error): Arrays are zero-indexed, so index 3 is out of bounds.
-friends[2] = "Haziq"; // Correcting the error by accessing a valid index.
+friends[3] = "Haziq"; // Correcting the error by accessing a valid index.
+console.log(friends);
--- Day 08/q22/main.ts
+++ Day 08/q22/main.ts
@@ -1,4 +1,5 @@
 let friends: string[] = ["Salman", "Da'ood", "Yousuf"];
 
 console.log(friends[3]); // Intentional error (Out Of Bound Error): Arrays are zero-indexed, so index 3 is out of bounds.
-friends[2] = "Haziq"; // Correcting the error by accessing a valid index.
+friends[3] = "Haziq"; // Correcting the error by accessing a valid index.
+console.log(friends)
