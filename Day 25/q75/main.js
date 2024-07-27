d626c2f3d50e6d2087ee09d8d37f87bb3e793e32
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2024-07-27 20:15:56 +0500

    first commit

---
+++ Day 25/q75/main.js
@@ -0,0 +1,6 @@
+var x = 4;
+console.log("Value of x: 4");
+console.log("Addition:", x += 2);
+console.log("Subtraction:", x -= 3);
+console.log("Multiplication:", x *= 4);
+console.log("Division:", x /= 6);
---
+++ Day 25/q75/main.ts
@@ -0,0 +1,7 @@
+let x = 4;
+console.log("Value of x: 4")
+
+console.log("Addition:",x += 2)
+console.log("Subtraction:",x -= 3);
+console.log("Multiplication:",x *= 4);
+console.log("Division:",x /= 6);
